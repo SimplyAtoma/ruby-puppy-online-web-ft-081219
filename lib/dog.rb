d8915eb2e0f078f444723f858a0270e2f5d03649
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-28 18:23:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,12 @@
 # Add your code here
 class Dog
   @@all = []
-  def initialize
-    @@all << self 
+  def initialize(name)
+    @@all << self
+    @name = name
+  end
+  
+  def self.all 
+    @@all
   end
 end
